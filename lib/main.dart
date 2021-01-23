d26fa1503012265ab0c090cffac0f8976192cf7c
Hiroto-Terai <hiro08051610@gmail.com>
2021-01-23 15:13:03 +0900

    update: 左右の余白を確保

--- lib/main.dart
+++ lib/main.dart
@@ -43,6 +43,11 @@ class WelcomePage extends StatelessWidget {
             children: [
               // ここにコンポーネントを置いていく
               _Header(),
+              Padding(
+                // 左右対象に余白を確保
+                padding: const EdgeInsets.symmetric(horizontal: 32),
+                child: _SignInForm(),
+              ),
             ],
           ),
         ),
@@ -252,6 +257,138 @@ class _Header extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+}
+
+// フォーム
+class _CustomTextField extends StatelessWidget {
+  final String labelText;
+  final String hintText;
+  final bool obscureText;
+
+  const _CustomTextField({
+    Key key,
+    @required this.labelText,
+    @required this.hintText,
+    @required this.obscureText,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      // 入力の装飾
+      decoration: InputDecoration(
+        // ラベルテキスト
+        labelText: labelText,
+        // プレースホルダー
+        hintText: hintText,
+        hintStyle: TextStyle(color: kTextColorSecondary),
+        // ラベル表示の方法
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        // フォーカスした時のフォームの振る舞い
+        focusedBorder: OutlineInputBorder(
+          // 角の丸み
+          borderRadius: BorderRadius.circular(8),
+          // ボーダの色
+          borderSide: BorderSide(
+            color: kAccentColor,
+          ),
+        ),
+        // フォーカスしていない時のフォームの振る舞い
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: BorderSide(
+            color: kTextColorSecondary,
+          ),
+        ),
+      ),
+      obscureText: obscureText,
+      onTap: () {},
+    );
+  }
+}
+
+class _SignInForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        _CustomTextField(
+          labelText: 'Email',
+          hintText: 'your email address goes here',
+          obscureText: false,
+        ),
+        SizedBox(height: 48),
+        _CustomTextField(
+          labelText: 'Password',
+          hintText: 'your password goes here',
+          obscureText: true,
+        ),
+        SizedBox(height: 4),
+        Text(
+          'Forgot Password?',
+          style: Theme.of(context)
+              .textTheme
+              .bodyText2
+              .copyWith(color: kTextColorSecondary),
+        ),
+        SizedBox(height: 48),
+        Container(
+          width: double.infinity,
+          child: FlatButton(
+            color: kButtonColorPrimary,
+            splashColor: kButtonTextColorPrimary,
+            padding: EdgeInsets.symmetric(vertical: 16),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+            onPressed: () {},
+            child: Text(
+              'Sign in',
+              style: Theme.of(context)
+                  .textTheme
+                  .button
+                  .copyWith(color: kButtonTextColorPrimary, fontSize: 18),
+            ),
+          ),
+        ),
+        SizedBox(height: 16),
+        Text(
+          'OR',
+          style: Theme.of(context)
+              .textTheme
+              .bodyText2
+              .copyWith(color: kTextColorSecondary),
+        ),
+        SizedBox(height: 16),
+        Text(
+          'Connect with',
+          style: Theme.of(context)
+              .textTheme
+              .bodyText2
+              .copyWith(color: kTextColorPrimary),
+        ),
+        SizedBox(height: 24),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconButton(
+              icon: Icon(Icons.account_circle),
+              onPressed: () {},
+            ),
+            Container(
+              color: kTextColorSecondary,
+              width: 1,
+              height: 16,
+            ),
+            IconButton(
+              icon: Icon(Icons.account_circle),
+              onPressed: () {},
+            ),
+          ],
+        )
+      ],
     );
   }
 }
